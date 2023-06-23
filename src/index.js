cbc713341499824046d41718f1e43afb1ae2aa1a
Zed <zaid.iqbal.777@gmail.com>
2023-06-23 10:31:58 +0500

    "small changes"

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import { Provider } from 'react-redux'
 import App from './App';
 import store from './app/store'
 
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-
-if (process.env.NODE_ENV === 'production') disableReactDevTools();
-
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
